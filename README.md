dc056c9a75fd0dd89089076e2d953d8d10946406
Damon Oehlman <damon.oehlman@gmail.com>
2013-10-18 20:43:48 +1100

    Configuration tweaks, gendocs

--- README.md
+++ README.md
@@ -2,8 +2,3 @@
 
 Ratchet assists with the process of dissecting CSS3 transform strings into
 javascript objects that you can then do something more intelligent with.
-
-[
-![Build Status]
-(https://travis-ci.org/DamonOehlman/ratchet.png?branch=master)
-](https://travis-ci.org/DamonOehlman/ratchet)
--- index.js
+++ index.js
@@ -7,11 +7,6 @@
   Ratchet assists with the process of dissecting CSS3 transform strings into
   javascript objects that you can then do something more intelligent with.
 
-  [
-  ![Build Status]
-  (https://travis-ci.org/DamonOehlman/ratchet.png?branch=master)
-  ](https://travis-ci.org/DamonOehlman/ratchet)
-
 **/
 
 var RatchetTransform = require('./types/transform');
