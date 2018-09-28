dc927a9c9f3526a98a25305527085c6154585073
Damon Oehlman <damon.oehlman@gmail.com>
2018-09-28 11:55:40 +1000

    .

--- examples/cam-test.js
+++
@@ -1,6 +0,0 @@
-var ratchet = require('..');
-var parsed = ratchet.fromString('translate3d(63.41263132471926px, 360.74743526062434px, 0px) rotate(0deg)');
-
-// parsed.translate.y.value += 10;
-console.log(parsed);
-console.log(parsed.toString());
